6c14329e0e813917e58fc2f3c07c9a4c89fbe49e
Mihretf <158094064+Mihretf@users.noreply.github.com>
2025-07-03 18:28:45 +0300

    Create ArithmeticOperators.py

---
+++ ArithmeticOperators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sum_of_num= a+b
+    difference_of_num= a-b
+    product_of_num= a *b
+    print(sum_of_num)
+    print(difference_of_num)
+    print(product_of_num)
